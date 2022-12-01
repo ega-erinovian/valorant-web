a5476041e01a88200d9d5828995cd78d186a8e80
ega-erinovian <erinovian69@gmail.com>
2022-12-01 13:28:04 +0700

    footer component finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { FaInstagram, FaLinkedin, FaGithub, FaGlobe } from 'react-icons/fa';
+
 // Import css
 import "./Footer.css"
 
@@ -7,8 +9,24 @@ const Footer = () => {
   return (
     <>
         <footer>
-            <div className="footer-content">
-                
+            <div className="footer-content container">
+                <h1>VALORANT<br/><span>AGENT</span></h1>
+                <ul>
+                    <li>
+                        <a href="#"><FaInstagram /></a>
+                    </li>
+                    <li>
+                        <a href="#"><FaLinkedin /></a>
+                    </li>
+                    <li>
+                        <a href="#"><FaGithub /></a>
+                    </li>
+                    <li>
+                        <a href="#"><FaGlobe /></a>
+                    </li>
+                </ul>
+                <p>All the images and videos are property of Riot Games™</p><br/>
+                <p><strong>2022 || ega-erinovian</strong></p>
             </div>
         </footer>
     </>
